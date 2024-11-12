e5c972b9e917adb2b36bc8b2685394915043e362
Sizzly <rob@robtacey.com>
2024-11-12 07:33:23 -0500

    #49 Front Carousel code sorted.

--- views/Front/featured_set_slideshow_html.php
+++ views/Front/featured_set_slideshow_html.php
@@ -1,135 +1,51 @@
 <?php
 /** ---------------------------------------------------------------------
- * themes/default/Front/front_page_html : Front page of site 
- * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
- * ----------------------------------------------------------------------
- *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * @package CollectiveAccess
- * @subpackage Core
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
- *
+ * themes/default/Front/featured_set_slideshow_html : Featured models carousel 
  * ----------------------------------------------------------------------
  */
 	$va_access_values = $this->getVar("access_values");
 	$qr_res = $this->getVar('featured_set_items_as_search_result');
 	$o_config = $this->getVar("config");
-	$vs_caption_template = $o_config->get("front_page_set_item_caption_template");
-	if(!$vs_caption_template){
-		$vs_caption_template = "<l>^ca_objects.preferred_labels.name</l>";
-	}
+	$vs_img_template = "^ca_object_representations.media.widepreview.url";
 	if($qr_res && $qr_res->numHits()){
 ?>   
-		<div class="jcarousel-wrapper">
-			<!-- Carousel -->
-			<div class="jcarousel">
-				<ul>
+
+<section id="featuredCarousel" class="py-5 py-md-14 py-lg-12">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-5">
+                <h2 class="fs-10 fw-bolder">Featured Models</h2>
+                <p class="fs-4 mb-0">
+                    Models of which I am especially proud.
+                </p>
+            </div>
+        </div>
+		<div class="owl-carousel leadership-carousel owl-theme mt-5">
+				
 <?php
-					while($qr_res->nextHit()){
-						if($qr_res->get("ca_object_representations.media.large")){
-							if($vs_media = $qr_res->getWithTemplate('<l>^ca_object_representations.media.large</l>', array("checkAccess" => $va_access_values))){
-								print "<li><div class='frontSlide'>".$vs_media;
-								$vs_caption = $qr_res->getWithTemplate($vs_caption_template);
-								if($vs_caption){
-									#print "<div class='frontSlideCaption'>".$vs_caption."</div>";
-								}
-								print "</div></li>";
-								$vb_item_output = true;
-							}
-						}
+			while($qr_res->nextHit()){
+				if($qr_res->get("ca_object_representations.media.widepreview")){
+					if($vs_media = $qr_res->getWithTemplate("$vs_img_template", array("checkAccess" => $va_access_values))){
+?>
+                        <div class="item">
+                            <a href="/index.php/Detail/objects/<?php print $qr_res->get("ca_objects.object_id"); ?>">
+                                <div class="">
+                                    <img src="<?php print $vs_media; ?>" alt="Featured Model" class="rounded-3">
+                                    <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
+                                        <h4 class="fs-5 fw-semibold mb-2"><?php print $qr_res->get("ca_objects.preferred_labels.name"); ?></h4>
+                                        <!-- <p class="fs-3 mb-0">CTO & Co-Founder</p> -->
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+<?php
 					}
-?>
-				</ul>
-			</div><!-- end jcarousel -->
-<?php
-			if($vb_item_output){
-?>
-			<!-- Prev/next controls -->
-			<a href="#" class="jcarousel-control-prev"><i class="fa fa-angle-left" aria-label="<?php print _t("Previous"); ?>"></i></a>
-			<a href="#" class="jcarousel-control-next"><i class="fa fa-angle-right" aria-label="<?php print _t("Next"); ?>"></i></a>
-		
-			<!-- Pagination -->
-			<p class="jcarousel-pagination">
-			<!-- Pagination items will be generated in here -->
-			</p>
-<?php
+				}
 			}
 ?>
-		</div><!-- end jcarousel-wrapper -->
-		<script type='text/javascript'>
-			jQuery(document).ready(function() {
-				/*
-				Carousel initialization
-				*/
-				$('.jcarousel')
-					.jcarousel({
-						// Options go here
-						wrap:'circular'
-					});
-		
-				/*
-				 Prev control initialization
-				 */
-				$('.jcarousel-control-prev')
-					.on('jcarouselcontrol:active', function() {
-						$(this).removeClass('inactive');
-					})
-					.on('jcarouselcontrol:inactive', function() {
-						$(this).addClass('inactive');
-					})
-					.jcarouselControl({
-						// Options go here
-						target: '-=1'
-					});
-		
-				/*
-				 Next control initialization
-				 */
-				$('.jcarousel-control-next')
-					.on('jcarouselcontrol:active', function() {
-						$(this).removeClass('inactive');
-					})
-					.on('jcarouselcontrol:inactive', function() {
-						$(this).addClass('inactive');
-					})
-					.jcarouselControl({
-						// Options go here
-						target: '+=1'
-					});
-		
-				/*
-				 Pagination initialization
-				 */
-				$('.jcarousel-pagination')
-					.on('jcarouselpagination:active', 'a', function() {
-						$(this).addClass('active');
-					})
-					.on('jcarouselpagination:inactive', 'a', function() {
-						$(this).removeClass('active');
-					})
-					.jcarouselPagination({
-						// Options go here
-					});
-			});
-		</script>
+
+	</div><!-- end owl carousel-wrapper -->
+</section>
 <?php
 	}
 ?>
--- views/Front/front_page_html.php
+++ views/Front/front_page_html.php
@@ -23,10 +23,8 @@
                     <p class="text-muted fs-5 mb-0 fw-bold text-center">The canonical archive of my miniature wargaming collection.</p>
                 </div>
             </div>
-        
-
             <div class="row align-items-end mb-3">
-                <div class="col-lg-3 d-none d-lg-block">
+                <div class="col-lg-6 d-none d-lg-block">
                     <div class="card text-white bg-primary rounded">
                         <div class="card-body p-4">
                             <span>
@@ -38,44 +36,6 @@
                             </p>
                         </div>
                     </div>
-                </div>
-
-
-                <div class="col-lg-6">
-
-                <div class="card w-auto">
-    <div class="card-body">
-      <div class="d-flex align-items-start">
-        <div class="bg-danger-subtle text-danger d-inline-block px-4 py-3 rounded">
-          <i class="ti ti-droplet-question display-1"></i>
-        </div>
-        <div class="ms-auto">
-          <div class="dropdown dropstart">
-            <a href="javascript:void(0)" class="link text-dark" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-              <i class="ti ti-dots fs-7"></i>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <p class="ms-2">How complete is the archive? In other words, how close does this archive reflect my collection?</p>
-              <p class="ms-2">Right now? Not even remotely!</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mt-5">
-        <h4 class="card-title">Archive Completeness Monitor</h4>
-        <div class="progress mt-4 bg-light">
-          <div class="progress-bar text-bg-danger" style="width: 1%; height: 6px;" role="progressbar">
-            <span class="sr-only">50% Complete</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-                </div>
-
-
-                <div class="col-lg-3 d-none d-lg-block">
                     <div class="card text-white bg-success rounded">
                         <div class="card-body p-4">
                             <span>
@@ -85,6 +45,36 @@
                             <p class="card-text text-white-50 fs-3 fw-normal">
                                 Collection Cost
                             </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-start">
+                                <div class="bg-danger-subtle text-danger d-inline-block px-4 py-3 rounded">
+                                    <i class="ti ti-droplet-question display-5"></i>
+                                </div>
+                                <div class="ms-auto">
+                                    <div class="dropdown dropstart">
+                                        <a href="javascript:void(0)" class="link text-dark" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <i class="ti ti-help fs-7"></i>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <p class="ms-2">How complete is the archive? In other words, how close does this archive reflect my collection?</p>
+                                            <p class="ms-2">Right now? Not even remotely!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-5">
+                                <h4 class="card-title">Archive Completeness Monitor</h4>
+                                <div class="progress mt-4 bg-light">
+                                    <div class="progress-bar text-bg-danger" style="width: 1%; height: 6px;" role="progressbar">
+                                        <span class="sr-only">50% Complete</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -98,67 +88,7 @@
     <!-- ------------------------------------- -->
     <!-- Leadership Start -->
     <!-- ------------------------------------- -->
-    <section class="py-5 py-md-14 py-lg-12">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-5">
-                    <h2 class="fs-10 fw-bolder">Featured Models</h2>
-                    <p class="fs-4 mb-0">
-                        Models of which I am especially proud.
-                    </p>
-                </div>
-            </div>
-            <div class="owl-carousel leadership-carousel owl-theme mt-5">
-                <div class="item">
-                    <div class="">
-                        <img src="http://localhost/media/collectiveaccess/images/0/58831_ca_object_representations_media_1_medium.jpg" alt="leader" class="rounded-3">
-                        <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
-                            <h4 class="fs-5 fw-semibold mb-2">Alex Martinez</h4>
-                            <p class="fs-3 mb-0">CEO & Co-Founder</p>
-                        </div>
-                    </div>
-                </div>
-          <div class="item">
-            <a href="#">
-            <div class="">
-              <img src="/themes/caiTS/assets//images/frontend-pages/jordan.jpg" alt="leader" class="rounded-3">
-              <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
-                <h4 class="fs-5 fw-semibold mb-2">Jordan Nguyen</h4>
-                <p class="fs-3 mb-0">CTO & Co-Founder</p>
-              </div>
-            </div>
-            </a>
-          </div>
-          <div class="item">
-            <div class="">
-              <img src="/themes/caiTS/assets//images/frontend-pages/taylor.jpg" alt="leader" class="rounded-3">
-              <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
-                <h4 class="fs-5 fw-semibold mb-2">Taylor Roberts</h4>
-                <p class="fs-3 mb-0">Product Manager</p>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="">
-              <img src="/themes/caiTS/assets//images/frontend-pages/morgan.jpg" alt="leader" class="rounded-3">
-              <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
-                <h4 class="fs-5 fw-semibold mb-2">Morgan Patel</h4>
-                <p class="fs-3 mb-0">Lead Developer</p>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="">
-              <img src="/themes/caiTS/assets//images/frontend-pages/kiana.jpg" alt="leader" class="rounded-3">
-              <div class="position-relative leadership-card z-1 bg-white mt-n10 rounded py-3 px-8 mx-9 text-center shadow-sm">
-                <h4 class="fs-5 fw-semibold mb-2">Morgan Patel</h4>
-                <p class="fs-3 mb-0">Lead Developer</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-    </section>
+    <?php print $this->render("Front/featured_set_slideshow_html.php"); ?>
     <!-- ------------------------------------- -->
     <!-- Leadership end -->
     <!-- ------------------------------------- -->
