3542a75037df36d6829de9ee7f18d7dac9fd1667
Sizzly <rob@robtacey.com>
2024-11-15 20:52:36 -0500

    #58 The collection page is functional.

--- views/Collections/index_html.php
+++ views/Collections/index_html.php
@@ -2,32 +2,68 @@
 	$o_collections_config = $this->getVar("collections_config");
 	$qr_collections = $this->getVar("collection_results");
 ?>
-	<div class="row">
-		<div class='col-md-12 col-lg-12 collectionsList'>
-			<h1><?php print $this->getVar("section_name"); ?></h1>
-			<p><?php print $o_collections_config->get("collections_intro_text"); ?></p>
+<div class="body-wrapper">
+    <div class="container-fluid">
+        <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
+            <div class="card-body px-4 py-3">
+              	<div class="row align-items-center">
+                	<div class="col-9">
+                  		<nav aria-label="breadcrumb">
+                    		<ol class="breadcrumb">
+                      			<li class="breadcrumb-item">
+                        			<a class="text-muted text-decoration-none" href="/index.php">Home</a>
+                      			</li>
+                      			<li class="breadcrumb-item" aria-current="page">Collections</li>
+                    		</ol>
+                  		</nav>
+                	</div>
+                	<div class="col-3">
+                  		<div class="text-center mb-n5">
+                  	</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-list">
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="table-responsive border rounded">
+                  	<table class="table align-middle text-nowrap mb-0">
+                    	<thead>
+                      		<tr>
+								<th scope="col">Collections</th>
+								<th scope="col"></th>
+								<th scope="col"></th>
+								<th scope="col"></th>
+								<th scope="col"></th>
+                      		</tr>
+                    	</thead>
+                    	<tbody>
 <?php	
-	$vn_i = 0;
 	if($qr_collections && $qr_collections->numHits()) {
 		while($qr_collections->nextHit()) {
-			if ( $vn_i == 0) { print "<div class='row'>"; } 
-			print "<div class='col-sm-6'><div class='collectionTile'><div class='title'>".caDetailLink($this->request, $qr_collections->get("ca_collections.preferred_labels"), "", "ca_collections",  $qr_collections->get("ca_collections.collection_id"))."</div>";	
-			if (($o_collections_config->get("description_template")) && ($vs_scope = $qr_collections->getWithTemplate($o_collections_config->get("description_template")))) {
-				print "<div>".$vs_scope."</div>";
-			}
-			print "</div></div>";
-			$vn_i++;
-			if ($vn_i == 2) {
-				print "</div><!-- end row -->\n";
-				$vn_i = 0;
-			}
-		}
-		if (($vn_i < 2) && ($vn_i != 0) ) {
-			print "</div><!-- end row -->\n";
+?>
+<tr>
+	<td>
+		<div class="d-flex align-items-center">
+			<div class="ms-3">
+				<h4 class="mb-0"><?php print caDetailLink($this->request, $qr_collections->get("ca_collections.preferred_labels"), "", "ca_collections",  $qr_collections->get("ca_collections.collection_id")); ?></h4>
+				<p class="mb-0"><?php print $qr_collections->get("ca_collections.description"); ?></p>
+			</div>
+		</div>
+	</td><td></td><td></td><td></td><td></td>
+</tr>
+<?php
 		}
 	} else {
 		print _t('No collections available');
 	}
 ?>
-		</div>
-	</div>
+                    	</tbody>
+                  	</table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
