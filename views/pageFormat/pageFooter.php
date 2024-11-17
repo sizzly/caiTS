3aaae700686776a855431d96d59ccc8d4efb9baf
Sizzly <rob@robtacey.com>
2024-11-16 20:50:28 -0500

    #70 Restructuring files

--- views/pageFormat/pageFooter.php
+++ views/pageFormat/pageFooter.php
@@ -2,58 +2,21 @@
 /* ----------------------------------------------------------------------
  * views/pageFormat/pageFooter.php : 
  * ----------------------------------------------------------------------
- */
-	if ($this->request->getController() == "Front" || $this->request->getController() == "About") {
+  */
 ?>
-        <!-- Code for Front and About -->
-<?php
-    }else{
-?>
-        <!-- Code for other pages -->
-            </div>
-        </div>
-        <div class="dark-transparent sidebartoggler"></div>
-
-<?php
-    }
-?>
-    <!-- ------------------------------------- -->
-    <!-- Footer Start -->
-    <!-- ------------------------------------- -->
-    <footer>
-        <div class="container-fluid">
-            <div class="border-bottom">
-                <div class="row mb-sm-12 mb-4">
-
-                </div>
-            </div>
-            <div class="d-flex justify-content-between py-7 flex-md-nowrap flex-wrap gap-sm-0 gap-3">
-                <div class="d-flex gap-3 align-items-center">
-                    <img src="/themes/caiTS/assets/pawtucket/graphics/logos/itsicon.jpg" alt="icon">
-                    <p class="fs-4 mb-0">All rights reserved by RST.</p>
-                </div>
-            <div>
-            <p class="mb-0">Theme by <a target="_blank" href="https://adminmart.com/" class="text-primary link-primary">AdminMart</a>.</p>
-            <p class="mb-0">Powered by <a target="_blank" href="https://collectiveaccess.org/" class="text-primary link-primary">Collective Access</a>.</p>
-        </div>
-    </footer>
-    <!-- ------------------------------------- -->
-    <!-- Footer End -->
-    <!-- ------------------------------------- -->
-
-    <!-- Scroll Top -->
-    <a href="javascript:void(0)" class="top-btn btn btn-primary d-flex align-items-center justify-content-center round-54 p-0 rounded-circle">
-        <i class="ti ti-arrow-up fs-7"></i>
-    </a>
-
-    <script src="/themes/caiTS/assets/js/vendor.min.js"></script>
-    <script src="/themes/caiTS/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/themes/caiTS/assets/libs/simplebar/dist/simplebar.min.js"></script>
-    <script src="/themes/caiTS/assets/js/theme/app.dark.init.js"></script>
-    <script src="/themes/caiTS/assets/js/theme/theme.js"></script>
-    <script src="/themes/caiTS/assets/js/theme/app.min.js"></script>
-    <script src="/themes/caiTS/assets/js/frontend-landingpage/homepage.js"></script>
-    <script src="/themes/caiTS/assets/libs/owl.carousel/dist/owl.carousel.min.js"></script>
-    <script src="/themes/caiTS/assets/js/apps/productDetail.js"></script>
-</body>
+		</div>
+<!-- END #app -->
+	
+		<!-- ================== BEGIN core-js ================== -->
+		<script src="/themes/caiTS/assets/js/vendor.min.js"></script>
+		<script src="/themes/caiTS/assets/js/app.min.js"></script>
+		<!-- ================== END core-js ================== -->
+	
+		<!-- ================== BEGIN page-js ================== -->
+		<script src="/themes/caiTS/assets/plugins/@highlightjs/cdn-assets/highlight.min.js"></script>
+		<script src="/themes/caiTS/assets/js/demo/highlightjs.demo.js"></script>
+		<script src="/themes/caiTS/assets/js/demo/sidebar-scrollspy.demo.js"></script>
+		<script src="/themes/caiTS/assets/plugins/lity/dist/lity.min.js"></script>
+		<!-- ================== END page-js ================== -->
+	</body>
 </html>
--- views/pageFormat/pageHeader.php
+++ views/pageFormat/pageHeader.php
@@ -4,246 +4,92 @@
  * ----------------------------------------------------------------------
  */
 ?>
-
 <!DOCTYPE html>
-<html lang="en" dir="ltr" data-bs-theme="dark" data-color-theme="Blue_Theme" data-layout="vertical">
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title><?php print (MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name"); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+	
+		<!-- ================== BEGIN core-css ================== -->
+		<link href="/themes/caiTS/assets/css/vendor.min.css" rel="stylesheet" />
+		<link href="/themes/caiTS/assets/css/theme.css" rel="stylesheet" />
+		<link href="/themes/caiTS/assets/css/mods.css" rel="stylesheet" />
+		<link href="/themes/caiTS/assets/plugins//lity/dist/lity.min.css" rel="stylesheet" />
 
-<head>
-	<!-- Required meta tags -->
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!-- ================== END core-css ================== -->
+	
+	</head>
+	<body>
+<!-- BEGIN #app -->
+		<div id="app" class="app app-content-full-width">
 
-	<!-- Favicon icon-->
-	<link rel="shortcut icon" type="image/png" href="/themes/caiTS/assets/images/logos/favicon.png" />
-
-	<!-- Core Css -->
-	<link rel="stylesheet" href="/themes/caiTS/assets/css/styles.css" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />	
-	<link rel="stylesheet" href="/themes/caiTS/assets/libs/owl.carousel/dist/assets/owl.carousel.min.css" />
-
-	<title><?= (MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name"); ?></title>
-
-</head>
-
-<?php
-	if ($this->request->getController() == "Front" || $this->request->getController() == "About") {
-?>
-		<body>
-			<!-- Preloader -->
-			<div class="preloader">
-				<img src="/themes/caiTS/assets/images/logos/favicon.png" alt="loader" class="lds-ripple img-fluid" />
-			</div>
-			<!-- ------------------------------------- -->
-			<!-- Header Start -->
-			<!-- ------------------------------------- -->
-			<header class="header-fp p-0 w-100">
-				<nav class="navbar navbar-expand-lg bg-primary-subtle py-2 py-lg-10">
-					<div class="custom-container d-flex align-items-center justify-content-between">
-						<a href="/index.php" class="text-nowrap logo-img">
-							<span class="fw-bold fs-8 text-white">iToysoldiers</span>					
-							<!-- <img src="/themes/caiTS/assets/images/logos/itslogo2.png" class="dark-logo" alt="Logo-Dark" /> -->
-							<!-- <img src="/themes/caiTS/assets/images/logos/itslogo2.png" class="light-logo" alt="Logo-light" /> -->
-						</a>
-						<button class="navbar-toggler border-0 p-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-							<i class="ti ti-menu-2 fs-8"></i>
-						</button>
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav mx-auto mb-2 gap-xl-7 gap-8 mb-lg-0">
-								<li class="nav-item">
-									<a class="nav-link fs-4 fw-bold text-dark link-primary" href="/index.php/About/Index">About</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link fs-4 fw-bold text-dark link-primary" href="/index.php/Browse/objects">Browse</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link fs-4 fw-bold text-dark link-primary" href="/index.php/Search/advanced/objects">Advanced Search</a>
-								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link fs-4 fw-bold text-dark link-primary d-flex gap-2" href="/index.php/Collections/index">Collections
-								</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</nav>
-			</header>
-			<!-- ------------------------------------- -->
-			<!-- Header End -->
-			<!-- ------------------------------------- -->
-
-			<!-- ------------------------------------- -->
-			<!-- Responsive Sidebar Start -->
-			<!-- ------------------------------------- -->
-			<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-				<div class="offcanvas-header">
-					<a href="/index.php">
-						<span class="fw-bold fs-8 text-white">iToysoldiers</span>	
+<!-- BEGIN #header -->
+			<div id="header" class="app-header">
+<!-- BEGIN desktop-toggler -->
+				<div class="desktop-toggler">
+					<button type="button" class="menu-toggler" data-toggle-class="app-sidebar-collapsed" data-dismiss-class="app-sidebar-toggled" data-toggle-target=".app">
+						<span class="bar"></span>
+						<span class="bar"></span>
+						<span class="bar"></span>
+					</button>
+				</div>
+<!-- END desktop-toggler -->
+			
+<!-- BEGIN mobile-toggler -->
+				<div class="mobile-toggler">
+					<button type="button" class="menu-toggler" data-toggle-class="app-sidebar-mobile-toggled" data-toggle-target=".app">
+						<span class="bar"></span>
+						<span class="bar"></span>
+						<span class="bar"></span>
+					</button>
+				</div>
+<!-- END mobile-toggler -->
+			
+<!-- BEGIN brand -->
+				<div class="brand">
+					<a href="/" class="brand-logo">
+						<span class="brand-img">
+							<span class="brand-img-text text-theme">I</span>
+						</span>
+						<span class="brand-text">iToysoldiers</span>
 					</a>
-					<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 				</div>
-				<div class="offcanvas-body">
-					<ul class="list-unstyled ps-0">
-						<li class="mb-1">
-							<a href="/index.php/About/Index" class="px-0 fs-4 d-block text-dark link-primary w-100 py-2">
-								About
-							</a>
-						</li>
-						<li class="mb-1">
-							<a href="/index.php/Browse/objects" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
-								Browse
-							</a>
-						</li>
-						<li class="mb-1">
-							<a href="/index.php/Search/advanced/objects" class="px-0 fs-4 d-flex align-items-center justify-content-start gap-2 w-100 py-2 text-dark link-primary">
-								Advanced Search
-							</a>
-						</li>
-						<li class="mb-1">
-							<a href="/index.php/Collections/index" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
-								Collections
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<!-- ------------------------------------- -->
-			<!-- Responsive Sidebar End -->
-			<!-- ------------------------------------- -->
-<?php
-	}else{
-?>
-		<body class="link-sidebar">
-			<!-- Preloader -->
-			<div class="preloader">
-				<img src="/themes/caiTS/assets/images/logos/favicon.png" alt="loader" class="lds-ripple img-fluid" />
-			</div>
-		
-			<div id="main-wrapper">
-				<!-- Sidebar Start -->
-				<aside class="left-sidebar with-vertical">
-					<div>
-						<!-- ---------------------------------- -->
-						<!-- Start Vertical Layout Sidebar -->
-						<!-- ---------------------------------- -->
-						<div class="brand-logo d-flex align-items-center justify-content-between">
-							<a href="/index.php">
-								<span class="fw-bold fs-8 text-white">iTS</span>	
-							</a>
-							<a href="javascript:void(0)" class="sidebartoggler ms-auto text-decoration-none fs-5 d-block d-xl-none">
-								<i class="ti ti-x"></i>
+<!-- END brand -->
+			
+<!-- BEGIN menu -->
+ 				<div class="menu">
+ 					<nav class="nav">
+					 	<div class="menu-item dropdown">
+							<a href="#" data-toggle-class="app-header-menu-search-toggled" data-toggle-target=".app" class="menu-link">
+								<div class="menu-icon"><i class="bi bi-search nav-icon"></i></div>
 							</a>
 						</div>
+						<a class="nav-link" href="/">Home</a>
+  						<a class="nav-link" href="/index.php/About/Index">About</a>
+  						<a class="nav-link" href="/index.php/Collections/Index">Order of Battle</a>
+  						<a class="nav-link" href="/index.php/Browse/objects">Browse</a>
 
-						<nav class="sidebar-nav scroll-sidebar" data-simplebar>
-							<ul id="sidebarnav">
-								<!-- ---------------------------------- -->
-								<!-- Home -->
-								<!-- ---------------------------------- -->
-								<li class="nav-small-cap">
-									<i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-									<span class="hide-menu">Navigation</span>
-								</li>
-								<!-- ---------------------------------- -->
-								<!-- Dashboard -->
-								<!-- ---------------------------------- -->
-								<li class="sidebar-item">
-									<a class="sidebar-link" href="/index.php" aria-expanded="false">
-										<span>
-											<i class="ti ti-home"></i>
-										</span>
-										<span class="hide-menu">Home</span>
-									</a>
-								</li>
-								<li class="sidebar-item">
-								<a class="sidebar-link" href="/index.php/About/Index" aria-expanded="false">
-									<span>
-										<i class="ti ti-info-circle"></i>
-									</span>
-									<span class="hide-menu">About</span>
-								</a>
-								</li>
-								<li class="sidebar-item">
-								<a class="sidebar-link" href="/index.php/Browse/objects" aria-expanded="false">
-									<span>
-										<i class="ti ti-binoculars-filled"></i>
-									</span>
-									<span class="hide-menu">Browse</span>
-								</a>
-								</li>
-								<li class="sidebar-item">
-								<a class="sidebar-link" href="/index.php/Search/advanced/objects" aria-expanded="false">
-									<span>
-										<i class="ti ti-search"></i>
-									</span>
-									<span class="hide-menu">Advanced Search</span>
-								</a>
-								</li>
-								<li class="sidebar-item">
-								<a class="sidebar-link" href="/index.php/Collections/index" aria-expanded="false">
-									<span>
-										<i class="ti ti-sitemap"></i>
-									</span>
-									<span class="hide-menu">Collections</span>
-								</a>
-								</li>
-							</ul>
-						</nav>
-
-						<!-- ---------------------------------- -->
-						<!-- Start Vertical Layout Sidebar -->
-						<!-- ---------------------------------- -->
+					</nav>
+				</div>
+<!-- END menu -->
+				<!-- BEGIN menu-search -->
+				<form class="menu-search" role="search" action="<?php print caNavUrl($this->request, '', 'MultiSearch', 'Index'); ?>">
+					<div class="menu-search-container">
+						<div class="menu-search-icon"><i class="bi bi-search"></i></div>
+						<div class="menu-search-input">
+							<input type="text" class="form-control form-control-lg" id="headerSearchInput" placeholder="Search" name="search" autocomplete="off" />
+						</div>
+						<div class="menu-search-icon">
+							<a href="#" data-toggle-class="app-header-menu-search-toggled" data-toggle-target=".app"><i class="bi bi-x-lg"></i></a>
+						</div>
 					</div>
-				</aside>
-				<!--  Sidebar End -->
-
-
-				<div class="page-wrapper">
-					<!--  Header Start -->
-					<header class="topbar">
-						<div class="with-vertical">
-							<!-- ---------------------------------- -->
-							<!-- Start Vertical Layout Header -->
-							<!-- ---------------------------------- -->
-							<nav class="navbar navbar-expand-lg p-0">
-								<ul class="navbar-nav">
-									<li class="nav-item nav-icon-hover-bg rounded-circle ms-n2">
-										<a class="nav-link sidebartoggler" id="headerCollapse" href="javascript:void(0)">
-											<i class="ti ti-menu-2"></i>
-										</a>
-									</li>
-									<li class="nav-item nav-icon-hover-bg rounded-circle d-none d-lg-flex">
-										<a class="nav-link" href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#exampleModal">
-											<i class="ti ti-search"></i>
-										</a>
-									</li>
-								</ul>
-
-								<div class="d-block d-lg-none py-4">
-									<a href="../dark/index.html" class="text-nowrap logo-img">
-										<a href="/index.php">
-											<span class="fw-bold fs-8 text-white">iToysoldiers</span>	
-										</a>
-									</a>
-								</div>
-
-								<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-									<div class="d-flex align-items-center justify-content-between">
-										<a href="javascript:void(0)" class="nav-link nav-icon-hover-bg rounded-circle mx-0 ms-n1 d-flex d-lg-none align-items-center justify-content-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar" aria-controls="offcanvasWithBothOptions">
-											<i class="ti ti-align-justified fs-7"></i>
-										</a>
-										<ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
-											<!-- Intentionally Left Blank in case I want to add to it -->
-										</ul>
-									</div>
-								</div>
-							</nav>
-							<!-- ---------------------------------- -->
-							<!-- End Vertical Layout Header -->
-							<!-- ---------------------------------- -->
-						</div>
-					</header>
-					<!--  Header End -->
-<?php
-	}
-?>
+				</form>
+				<!-- END menu-search -->
+			</div>
+<!-- END #header -->
+		
+<!-- BEGIN #content -->
+			<div id="content" class="app-content">
