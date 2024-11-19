fa5d99ff9eba5db2e5e9338a4c1fd56e851a713e
Sizzly <rob@robtacey.com>
2024-11-19 15:06:45 -0500

    #68 Search Results page consistent with other pages.

--- views/Search/multisearch_results_html.php
+++ views/Search/multisearch_results_html.php
@@ -1,11 +1,13 @@
 <?php
 	$va_results = $this->getVar('results');
 	$va_result_count = $va_results['_info_']['totalCount'];
-	?>
+?>
+
 <ul class="breadcrumb">
 	<li class="breadcrumb-item"><a href="/">Home</a></li>
     <li class="breadcrumb-item" aria-current="page">Search Results</li>
 </ul>
+
 <h1 class="page-header"><?php print _t("Search results for %1", caUcFirstUTF8Safe($this->getVar('searchForDisplay'))); ?>
 </h1>
 <hr class="mb-4" />
@@ -13,7 +15,7 @@
 <div class="row">
 	<div class="col-md-3 mb-3">
 		<div class='card'>
-			<div class='card-header fw-bold'>SUMMARY</div>
+			<div class='card-header fw-bold small bg-white bg-opacity-15'>SUMMARY</div>
   			<div class='card-body'>
 <?php
 				$i = 0;
@@ -37,28 +39,25 @@
 	</div>
 	<div class="col-md-9 mb-3">
 		<div class='card'>
-  			<div class='card-header fw-bold'>RESULTS</div>
-  				<div class='card-body'>
+  			<div class='card-header fw-bold small bg-white bg-opacity-15'>RESULTS</div>
+			<div class='card-body'>
 <?php
-					// 
-					// Print out block content (results for each type of search)
-					//
-					foreach($this->getVar('blockNames') as $vs_block) {
+				foreach($this->getVar('blockNames') as $vs_block) {
 ?>
-						<a href='#' name='<?php print $vs_block; ?>' aria-label='<?php print $vs_block; ?>'></a>
-						<div id="<?php print $vs_block; ?>Block" class='resultBlock'>
-							<?php print $va_results[$vs_block]['html']; ?>
-						</div>
+					<a href='#' name='<?php print $vs_block; ?>' aria-label='<?php print $vs_block; ?>'></a>
+					<div id="<?php print $vs_block; ?>Block" class='resultBlock'>
+						<?php print $va_results[$vs_block]['html']; ?>
+					</div>
 <?php
-					} 
+				} 
 ?>
-  				</div>
-  				<div class='card-arrow'>
-    				<div class='card-arrow-top-left'></div>
-    				<div class='card-arrow-top-right'></div>
-    				<div class='card-arrow-bottom-left'></div>
-    				<div class='card-arrow-bottom-right'></div>
-  				</div>
+			</div>
+			<div class='card-arrow'>
+				<div class='card-arrow-top-left'></div>
+				<div class='card-arrow-top-right'></div>
+				<div class='card-arrow-bottom-left'></div>
+				<div class='card-arrow-bottom-right'></div>
 			</div>
 		</div>
 	</div>
+</div>
